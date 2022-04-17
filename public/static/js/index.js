335b09ba38f3a75119414f9537a1a02624f5b890
Alberto Hinojosa <ahinojosa721@gmail.com>
2022-04-17 00:10:42 -0500

    dark-light toggle button

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,21 +1,52 @@
+let addToCart = 0;
 document.addEventListener('DOMContentLoaded', (event) => {
-    fetch('https://testimonialapi.toolcarton.com/api')
+  addingEventListener();
+  fetch('https://testimonialapi.toolcarton.com/api')
     .then(response => response.json())
     .then(data => {
-      setInterval(function() {
+      setInterval(function () {
         rotateTestimonials(data);
       }, 5000);
     });
 });
 
 function rotateTestimonials(_payload) {
-    var i = Math.floor(Math.random() * 10) % _payload.length;
-    var testimonial = _payload[i]
-    var message = testimonial.message;
-    var name = testimonial.name;
-    var location = testimonial.location;
+  var i = Math.floor(Math.random() * 10) % _payload.length;
+  var testimonial = _payload[i]
+  var message = testimonial.message;
+  var name = testimonial.name;
+  var location = testimonial.location;
 
-    document.getElementById("name").innerText = name;
-    document.getElementById("location").innerText = location;
-    document.getElementById("testimonial").innerText = message;
+  document.getElementById("name").innerText = name;
+  document.getElementById("location").innerText = location;
+  document.getElementById("testimonial").innerText = message;
+}
+function addingEventListener() {
+  var elements = document.getElementsByClassName("add-cart");
+
+  function clickAddToCart() {
+    addToCart += 1;
+    document.getElementById("cart").innerText = "Cart(" + addToCart + ")";
+    document.getElementById("emptyCart").style.opacity = 1;
+  }
+
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', clickAddToCart);
+  }
+
+  document.getElementById("emptyCart").addEventListener("click", clearCart);
+
+  const btn = document.querySelector('.btn-toggle');
+  // Listen for a click on the button
+  btn.addEventListener('click', function () {
+    // Then toggle (add/remove) the .dark-theme class to the body
+    console.log("In the event listener")
+    document.body.classList.toggle('dark-theme');
+  })
+}
+
+function clearCart() {
+  addToCart = 0;
+  document.getElementById("cart").innerText = "Cart";
+  document.getElementById("emptyCart").style.opacity = 0;
 }
