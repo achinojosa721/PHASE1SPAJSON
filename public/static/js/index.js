aeed63096c6de10bcb90fa5d67c7d5cbc5add162
Alberto Hinojosa <ahinojosa721@gmail.com>
2022-04-21 22:29:44 -0500

    commit commented out listen for click on button

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -37,10 +37,7 @@ function addingEventListener() {
   document.getElementById("emptyCart").addEventListener("click", clearCart);
 
   const btn = document.querySelector('.btn-toggle');
-  // Listen for a click on the button
   btn.addEventListener('click', function () {
-    // Then toggle (add/remove) the .dark-theme class to the body
-    console.log("In the event listener")
     document.body.classList.toggle('dark-theme');
   })
 }
